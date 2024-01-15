e95d45074e6c5196109de2143463d9455e176840
yughh <108441610+yughh@users.noreply.github.com>
2024-01-15 12:38:26 +0900

    [level Lv.0] Title: 양꼬치, Time: 0.02 ms, Memory: 82.3 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = n*12000 + k*2000;
+        if(n >= 10) answer =answer -(n/10) *2000;
+        return answer;
+    }
+}
