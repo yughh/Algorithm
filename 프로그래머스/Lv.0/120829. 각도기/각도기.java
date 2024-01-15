b5773c2a228e81729b49e5aecff8347a58d1d13a
yughh <108441610+yughh@users.noreply.github.com>
2024-01-15 11:51:23 +0900

    [level Lv.0] Title: 각도기, Time: 0.03 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/120829. 각도기/각도기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int angle) {
+        int answer = 0;
+        if(angle>0 && angle <90){
+            return answer=1;
+        }else if( angle == 90){
+             return answer=2;
+        }else if (angle<180){
+            return answer=3;
+        } else{
+              return answer=4;
+        }
+      
+    }
+}
