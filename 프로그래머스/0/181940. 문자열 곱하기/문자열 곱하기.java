8fd9163bf6d1d4a34682b7453bb639e7e243c982
yughh <108441610+yughh@users.noreply.github.com>
2024-07-09 01:24:19 +0900

    [level 0] Title: 문자열 곱하기, Time: 1.52 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String my_string, int k) {
+        String answer = "";
+        while(k > 0){
+            answer += my_string;
+            k--;
+        }
+        return answer;
+    }
+}
