a29202c8981b1fecc3b3fe7a7d399fcfab4d0ad6
yughh <108441610+yughh@users.noreply.github.com>
2024-07-13 09:54:28 +0900

    [level 0] Title: 배열 만들기 3, Time: 2.03 ms, Memory: 106 MB -BaekjoonHub

---
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] intervals) {
+        int[] answer = new int [intervals[0][1]-intervals[0][0] + intervals[1][1]-intervals[1][0]+2];
+        int j = 0;
+        for(int i=intervals[0][0]; i<= intervals[0][1]; i++){
+            answer[j] = arr[i];
+            j++;
+        }
+        
+        for(int i=intervals[1][0]; i<= intervals[1][1]; i++){
+           answer[j] = arr[i];
+           if(j < answer.length-1)j++;
+        }
+        return answer;
+    }
+}
