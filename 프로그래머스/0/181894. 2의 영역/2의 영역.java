0a68fb97383daf5aaf43daef5fe4d5f613db32bb
yughh <108441610+yughh@users.noreply.github.com>
2024-07-14 23:57:42 +0900

    [level 0] Title: 2의 영역, Time: 18.16 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.java
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.java
@@ -1,35 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int[] arr) {
-       // int[] answer = {};
-        List<Integer> list = new ArrayList();
-        int a = 0;
-        for(int i=0; i<arr.length; i++){
-            if(arr[i] ==2 || a>0){
-                if(arr[i] ==2) a++;
-                list.add(arr[i]);
-                //if(a >= 2) break;
-            }
-        }
-        if(a ==1){
-        int[] answer = {list.get(0)};
-        return answer; 
-            
-        }else if(a>=1){
-            //2~2
-            for(int i=list.size()-1; i>= 0; i--){
-                if(list.get(i) == 2){
-                    break;
-                }else{
-                    list.remove(i);
-                }
-            }
-            return list.stream().mapToInt(i->i).toArray();
-        }else{
-            int[] answer = {-1};
-           return answer;
-        }
-        
+        int[] answer = {};
+        return answer;
     }
 }
