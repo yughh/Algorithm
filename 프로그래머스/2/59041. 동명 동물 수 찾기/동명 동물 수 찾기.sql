128ce0fafff6b5811432f1f5e74d0bd7ed7ce6ad
yughh <108441610+yughh@users.noreply.github.com>
2024-08-01 00:10:56 +0900

    [level 2] Title: 동명 동물 수 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59041. 동명 동물 수 찾기/동명 동물 수 찾기.sql
@@ -0,0 +1,9 @@
+-- 코드를 입력하세요
+-- 동물 보호소에 들어온 동물 이름 중 두 번 이상 쓰인 이름과 해당 이름이 쓰인 횟수를 조회하는 SQL문을 작성해주세요
+-- 이때 결과는 이름이 없는 동물은 집계에서 제외하며, 결과는 이름 순으로 조회해주세요.
+SELECT name, count(name) as count
+from ANIMAL_INS
+where NAME is not null
+group by name
+having count(name)> 1
+order by NAME;
