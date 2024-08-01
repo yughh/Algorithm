db761f0d6c0427e3a0c84ed2591d9ecbd86ff510
yughh <108441610+yughh@users.noreply.github.com>
2024-08-01 15:09:57 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -1,8 +1,8 @@
 select b.TITLE, b.BOARD_ID,
        r.REPLY_ID, r.WRITER_ID, r.CONTENTS , 
-       to_char(r.CREATED_DATE, 'YYYY-MM-DD') as CREATED_DATE
+       Date_Format(r.CREATED_DATE, '%Y-%m-%d') as CREATED_DATE
 from USED_GOODS_BOARD b 
 Inner Join USED_GOODS_REPLY r
 on b.BOARD_ID = r.BOARD_ID
-where b.CREATED_DATE  Between to_date('2022-10-01', 'YYYY-MM-DD') and to_date('2022-10-31', 'YYYY-MM-DD')
+where b.CREATED_DATE  Between Date_Format('2022-10-01', '%Y-%m-%d') and Date_Format('2022-10-31', '%Y-%m-%d')
 order by r.CREATED_DATE asc, b.TITLE asc;
