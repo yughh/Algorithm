fedf72f1213a047a7dadfae096eb8c8e02b9b2fd
yughh <108441610+yughh@users.noreply.github.com>
2024-08-01 15:18:09 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -1,12 +1,9 @@
--- USED_GOODS_BOARD 테이블에서
---2022년 10월 5일에 등록된 
---중고거래 게시물의 게시글 ID, 작성자 ID, 게시글 제목, 가격, 거래상태를 조회하는 SQL문을 작성해주세요. 
---거래상태가 SALE 이면 판매중, RESERVED이면 예약중, DONE이면 거래완료 분류하여 출력해주시고, 결과는 게시글 ID를 기준으로 내림차순 정렬해주세요.
 SELECT BOARD_ID, WRITER_ID, TITLE, PRICE 
-        ,decode(STATUS,
-              'SALE', '판매중',
-              'RESERVED', '예약중',
-              '거래완료') as "STATUS"
+ ,CASE
+        WHEN STATUS = 'DONE' THEN '거래완료'
+        WHEN STATUS = 'SALE' THEN '판매중'
+        ELSE '예약중'
+    END AS STATUS
 from USED_GOODS_BOARD
-where to_char(CREATED_DATE, 'YYYY-MM-DD') = '2022-10-05'
+where Date_Format(CREATED_DATE, '%Y-%m-%d') = '2022-10-05'
 order by BOARD_ID desc;
