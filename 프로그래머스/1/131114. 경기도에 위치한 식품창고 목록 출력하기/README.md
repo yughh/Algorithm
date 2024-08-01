75b793fa16033a560a1d458cf6a955ebd2751d85
yughh <108441610+yughh@users.noreply.github.com>
2024-08-01 16:11:15 +0900

    [level 1] Title: 경기도에 위치한 식품창고 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/131114. 경기도에 위치한 식품창고 목록 출력하기/README.md
+++ 프로그래머스/1/131114. 경기도에 위치한 식품창고 목록 출력하기/README.md
@@ -1,6 +1,6 @@
 # [level 1] 경기도에 위치한 식품창고 목록 출력하기 - 131114 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131114) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131114?language=mysql) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 07월 31일 06:11:11
+2024년 08월 01일 16:11:13
 
 ### 문제 설명
 
--- 프로그래머스/1/131114. 경기도에 위치한 식품창고 목록 출력하기/경기도에 위치한 식품창고 목록 출력하기.sql
+++ 프로그래머스/1/131114. 경기도에 위치한 식품창고 목록 출력하기/경기도에 위치한 식품창고 목록 출력하기.sql
@@ -1,9 +1,7 @@
--- FOOD_WAREHOUSE 테이블에서 경기도에 위치한 창고의 ID, 이름, 주소, 냉동시설 여부를 조회하는 SQL문을 작성해주세요.
--- 이때 냉동시설 여부가 NULL인 경우, 'N'으로 출력시켜 주시고 결과는 창고 ID를 기준으로 오름차순 정렬해주세요.
 SELECT WAREHOUSE_ID
         ,WAREHOUSE_NAME
         ,ADDRESS
-        ,NVL2(replace(FREEZER_YN, 'N', null), 'Y', 'N') as FREEZER_YN
+        ,IFNULL(FREEZER_YN, 'N') as FREEZER_YN
 from FOOD_WAREHOUSE
 where ADDRESS Like '%경기도%'
 order by WAREHOUSE_ID;
