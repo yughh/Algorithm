10c51600172805770dcf53086d9425eb2df81ae7
yughh <108441610+yughh@users.noreply.github.com>
2024-08-02 19:24:44 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -0,0 +1,13 @@
+-- 관리자의 실수로 일부 동물의 입양일이 잘못 입력되었습니다. 
+-- 보호 시작일보다 입양일이 더 빠른 동물의 아이디와 이름을 조회하는 SQL문을 작성해주세요
+-- . 이때 결과는 보호 시작일이 빠른 순으로 조회해야합니다.
+SELECT o.ANIMAL_ID, o.NAME
+from ANIMAL_OUTS o join ANIMAL_INS i
+on o.ANIMAL_ID = i.ANIMAL_ID
+where TIMESTAMPDIFF(second, i.DATETIME, o.datetime) < 0
+order by i.DATETIME ;
+# SELECT o.ANIMAL_ID, o.NAME, i.DATETIME, o.datetime
+# from ANIMAL_OUTS o join ANIMAL_INS i
+# on o.ANIMAL_ID = i.ANIMAL_ID
+# where DATEDIFF(o.DATETIME, i.DATETIME ) < 0
+# order by i.DATETIME  ;
