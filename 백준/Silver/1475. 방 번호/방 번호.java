ef4de4b2fec8a6e10f5c5b3eb2f04eb9caf7aed7
yughh <108441610+yughh@users.noreply.github.com>
2024-10-30 00:21:03 +0900

    [Silver V] Title: 방 번호, Time: 176 ms, Memory: 17692 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.java
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -1,23 +1,23 @@
 import java.util.*;
 
-class Main {
-    public static void main(String[] arg) {
-        Scanner sc = new Scanner(System.in);
-        String roomNumber = sc.nextLine();
-        int[] count = new int[10];
+class Main{
+    public static void main (String[] arg){
+    	Scanner sc = new Scanner(System.in);
+        String[] arr = sc.nextLine().split("");
+        int[] n = new int[10];
+        int answer = 0;
+      
         
-        // 각 숫자의 개수를 세기
-        for (char ch : roomNumber.toCharArray()) {
-            count[ch - '0']++;
+        for(int i=0; i<arr.length; i++){
+        	int num = Integer.parseInt(arr[i]);
+            n[num]++;
         }
         
-        // 6과 9는 대체 가능하므로 합쳐서 필요한 세트를 계산
-        count[6] = (count[6] + count[9] + 1) / 2;
-
-        // 나머지 숫자 중 가장 많이 필요한 세트 수를 계산
-        int answer = 0;
-        for (int i = 0; i < 9; i++) {  // 6과 9를 합쳤으므로, 0 ~ 8만 체크
-            answer = Math.max(answer, count[i]);
+        int t = (n[9]+n[6] +1 )/2;
+        n[9] = t;
+        n[6] = t;
+        for(int i=0; i<n.length; i++) {
+        	if(answer < n[i]) answer = n[i];
         }
         
         System.out.println(answer);
