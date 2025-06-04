2e2021ab8d7fc43cae1e25ff1a13b0b0298c5d46
yughh <108441610+yughh@users.noreply.github.com>
2025-06-04 14:54:24 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -1,11 +1,7 @@
--- 입양을 간 동물 중, 보호 기간이 가장 길었던 동물 두 마리의 아이디와 이름을 조회하는 SQL문을 작성해주세요
-#  이때 결과는 보호 기간이 긴 순으로 조회해야 합니다.
-select s.animal_id, s.name
-from (
-
-SELECT o.ANIMAL_ID,	o.NAME, timestampDiff(second, i.DATETIME, o.DATETIME) as t
-from ANIMAL_INS i right outer join ANIMAL_OUTS o
+-- 코드를 입력하세요
+select ANIMAL_ID, NAME
+from (SELECT i.name, i.animal_id, timestampdiff(second,i.DATETIME, o.DATETIME) as diff
+from ANIMAL_INS as i join ANIMAL_OUTS as o
 on i.ANIMAL_ID = o.ANIMAL_ID
-where i.animal_id is not null
-order by t desc limit 2
-    ) s ;
+order by diff desc
+limit 2) as t;
