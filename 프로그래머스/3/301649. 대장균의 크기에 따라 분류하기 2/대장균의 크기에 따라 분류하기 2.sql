5a8fcc13d4d5c8d48f4320d992343dd8c981bd83
yughh <108441610+yughh@users.noreply.github.com>
2025-06-05 14:58:53 +0900

    [level 3] Title: 대장균의 크기에 따라 분류하기 2, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
+++ 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
@@ -1,23 +1,15 @@
-# 대장균 개체의 크기를 내름차순으로 정렬했을 때 
-# 상위 0% ~ 25% 를 'CRITICAL', 
-# 26% ~ 50% 를 'HIGH', 
-# 51% ~ 75% 를 'MEDIUM',
-# 76% ~ 100% 를 'LOW' 라고 분류합니다.
-# 대장균 개체의 ID(ID) 와 분류된 이름(COLONY_NAME)을 출력하는 SQL 문을 작성해주세요.
-# 이때 결과는 개체의 ID 에 대해 오름차순 정렬해주세요 .
-# 단, 총 데이터의 수는 4의 배수이며 같은 사이즈의 대장균 개체가 서로 다른 이름으로 분류되는 경우는 없습니다.
-select 
-    s.id as ID
-    ,case   
-        when s.len <= 0.25 then 'CRITICAL'
-        when s.len <= 0.5 then 'HIGH'
-         when s.len <= 0.75 then 'MEDIUM'
-         else 'LOW'
-     end as COLONY_NAME
-from (
-    select 
-        PERCENT_RANK() over (order by SIZE_OF_COLONY desc) as len
-        ,id
-    from ECOLI_DATA
-) s
-order by s.id;
+-- 코드를 작성해주세요
+select
+t.ID
+,case 
+    when t.rnk between 1 and (Select count(*) from ECOLI_DATA)/4*1 then 'CRITICAL'
+     when t.rnk between  (Select count(*) from ECOLI_DATA)/4*1+1 and  (Select count(*) from ECOLI_DATA)/4*2 then 'HIGH'
+      when t.rnk between  (Select count(*) from ECOLI_DATA)/4*2+1 and (Select count(*) from ECOLI_DATA)/4*3 then 'MEDIUM'
+      else 'LOW'
+     END as 'COLONY_NAME'
+from (select 
+ID,SIZE_OF_COLONY
+,row_number() over(order by SIZE_OF_COLONY desc ) as rnk
+from ECOLI_DATA
+) as t 
+order by ID;
