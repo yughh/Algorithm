7a03db54e1eabde422f73e372b020b4f9765006a
yughh <108441610+yughh@users.noreply.github.com>
2025-06-05 13:09:11 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -1,21 +1,12 @@
--- USED_GOODS_BOARD와 USED_GOODS_USER 테이블에서
-# 중고 거래 게시물을 3건 이상 등록한 사용자의 사용자 ID, 닉네임, 전체주소, 전화번호를 조회하는 SQL문을 작성해주세요.
-#이때, 전체 주소는 시, 도로명 주소, 상세 주소가 함께 출력되도록 해주시고, 
-#전화번호의 경우 xxx-xxxx-xxxx 같은 형태로 하이픈 문자열(-)을 삽입하여 출력해주세요.
-# 결과는 회원 ID를 기준으로 내림차순 정렬해주세요.
-SELECT USER_ID
-        ,NICKNAME
-        # ,concat(concat(concat(concat(CITY, " "), STREET_ADDRESS1), " "), STREET_ADDRESS2) as `전체주소`
-        # ,concat(concat(concat(concat(substring(TLNO, 1, 3), "-"),  substring(TLNO, 4, 4)), "-"),  substring(TLNO, 8, 4)) as `전화전호`
-        ,concat(CITY, " ", STREET_ADDRESS1, " ", STREET_ADDRESS2) as `전체주소`
-        ,concat(substring(TLNO, 1, 3), "-",substring(TLNO, 4, 4), "-", substring(TLNO, 8, 4)) as `전화전호`
-        
-from  USED_GOODS_USER 
-where user_id in(
-    select WRITER_ID
-    from USED_GOODS_BOARD
-    Group by WRITER_ID
-    having count(WRITER_ID)>= 3
-)
-order by USER_ID desc
-;
+-- 코드를 입력하세요
+SELECT
+USER_ID
+,NICKNAME
+,concat(CITY, " ",STREET_ADDRESS1," ",STREET_ADDRESS2) as '전체주소'
+,concat(substring(TLNO, 1, 3),"-",substring(TLNO, 4, 4),"-",substring(TLNO, 8, 4)) as '전화번호'
+FROM USED_GOODS_USER
+where user_ID in (SELECT WRITER_ID
+from USED_GOODS_BOARD 
+group by WRITER_ID
+having count(WRITER_ID) >2)
+order by USER_ID desc;
