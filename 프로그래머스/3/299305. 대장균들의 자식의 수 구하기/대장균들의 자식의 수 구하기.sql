08678bb9bb8eb779e3dd584c2bd89001b3d3e71d
yughh <108441610+yughh@users.noreply.github.com>
2025-06-05 14:33:09 +0900

    [level 3] Title: 대장균들의 자식의 수 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/299305. 대장균들의 자식의 수 구하기/대장균들의 자식의 수 구하기.sql
+++ 프로그래머스/3/299305. 대장균들의 자식의 수 구하기/대장균들의 자식의 수 구하기.sql
@@ -1,28 +1,12 @@
--- 대장균 개체의 ID(ID)와 자식의 수(CHILD_COUNT)를 출력하는 SQL 문을 작성해주세요. 
-# 자식이 없다면 자식의 수는 0으로 출력해주세요. 이때 결과는 개체의 ID 에 대해 오름차순 정렬해주세요.
-# select d.id
-#         ,ifnull(cnt, 0) as CHILD_COUNT
-# from ECOLI_DATA d left outer join 
-# (
-#     select PARENT_ID
-#             ,count(PARENT_ID) as cnt 
-#     from ECOLI_DATA
-#     where PARENT_ID is not null
-#     group by PARENT_ID
-# ) s
-# on d.PARENT_ID = s.PARENT_ID
-# order by d.id;
-
-
-select d.id
-        ,ifnull(cnt, 0) as CHILD_COUNT
-from ECOLI_DATA d left outer join 
-(
-    select PARENT_ID
-            ,count(PARENT_ID) as cnt 
-    from ECOLI_DATA
-    where PARENT_ID is not null
-    group by PARENT_ID
-) s
-on d.id = s.PARENT_ID
-order by d.id;
+-- 코드를 작성해주세요
+select e.ID
+,IFNULL(CHILD_COUNT, 0) as CHILD_COUNT
+from ECOLI_DATA as e left join (select 
+p1.ID
+,count(p1.ID) as CHILD_COUNT
+from ECOLI_DATA  p1 left join ECOLI_DATA p2
+on p1.ID = p2.PARENT_ID
+where p2.PARENT_ID is not null
+group by p1.ID ) as t
+on e.id = t.id
+order by  e.ID;
